6b7b380130ed447ca79bece2abf12cf7e751a654
unknown <peluffojuancruz@gmail.com>
2022-07-12 09:51:45 -0300

    prod config

--- src/config/index.js
+++ src/config/index.js
@@ -1,19 +1,21 @@
 require('dotenv').config();
 
 const DEVELOPMENT = require('./development');
+const PRODUCTION = require('./production');
 const { NODE_ENV } = process.env;
 
 let Env;
 
 switch (NODE_ENV) {
-    case "development": {
+    case 'development':
         Env = DEVELOPMENT;
         break;
-    }
-    default: {
+    case 'production':
+        Env = PRODUCTION
+        break;
+    default:
         Env = DEVELOPMENT;
         break;
-    }
 }
 
 module.exports = Env;
